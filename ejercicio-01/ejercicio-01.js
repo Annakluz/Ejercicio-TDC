4a690e1a072f6ca93191864c048bd03933933c7f
Annakluz <anna.luz.duran@gmail.com>
2017-04-09 23:37:22 -0500

    subir repositorio

---
+++ ejercicio-01/ejercicio-01.js
@@ -0,0 +1,11 @@
+//VARIABLES GLOBALES
+
+function isValidCard(creditCardNumber) {
+    // Escribe tu código aquí
+    
+}
+
+// NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
+if (typeof exports !== 'undefined') {
+    exports.isValidCard = isValidCard;
+}
