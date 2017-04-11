9c6152cd64841465017af40fdb1db32e61d04814
Annakluz <anna.luz.duran@gmail.com>
2017-04-10 20:31:52 -0500

    dos pruebas

--- ejercicio-01/ejercicio-01.js
+++ ejercicio-01/ejercicio-01.js
@@ -2,7 +2,27 @@
 
 function isValidCard(creditCardNumber) {
     // Escribe tu código aquí
-    
+    if(creditCardNumber == undefined){ //TEST 1
+        return("Ingresa un numero");
+    }
+    if(typeof creditCardNumber != "number"){ //TEST 2
+        return("Error de dato");
+    }
+    if(numeros(creditCardNumber) = false){ //TEST 3
+        return("Faltan numeros");
+    //FUNCTION TEST3
+    function numeros(creditCardNumber){
+    let number = creditCardNumber.toString();
+    if(number.length == 16){
+        return(true);
+    }
+}
+    }
+    if(validarNumero(creditCardNumber) == true){ //TEST 4 y 5
+        return(true);
+    } else {
+        return (false);
+    }
 }
 
 // NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
